12e56c91c036fed9a98bd18454e1725677a8baee
jonathanramanujam <jonathanramanujam@mail.weber.edu>
2022-08-29 18:40:15 -0600

    Completed Part 2

--- part2.py
+++ part2.py
@@ -2,3 +2,26 @@
 # CS 3620
 # Python Basics
 # Part 2: List of Food Items
+
+favorite_foods = [  'chicken coconut korma',
+                    'sushi',
+                    'cheeseburger',
+                    'clam chowder in a bread bowl',
+                    'crab legs'
+                    ]
+
+print("\nFavorite Food #3: " + favorite_foods[2])
+
+print("\nAppending items...")
+favorite_foods.append("shredded pork pozole")
+favorite_foods.append("massaman curry")
+
+print("\nList of Favorite Foods: ")
+for food in favorite_foods:
+    print(food)
+
+print("\nInserting tacos...")
+favorite_foods.insert(3, "tacos")
+
+print("\nFinal List")
+print(favorite_foods)
